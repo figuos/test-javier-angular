11135fe30a97767abacc4dd002125eaf8a1b7517
figuos <omfr.13@gmail.com>
2018-03-01 15:22:07 -0400

    Initial commit

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
+// server.js
 const express = require('express');
 const app = express();
-
-app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8000);
+// If an incoming request uses
+// a protocol other than HTTPS,
+// redirect that request to the
+// same url but with HTTPS
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+// Instruct the app
+// to use the forceSSL
+// middleware
+app.use(forceSSL());
+// ...
